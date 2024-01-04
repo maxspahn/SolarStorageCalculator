b85988916cf05268f5316771cb209988e82c8352
Max Spahn <m.spahn@tudelft.nl>
2024-01-04 23:48:28 +0100

    ft[costs]: Adds cost to the calculation and units to text fields.

--- solarcalc/app.py
+++ solarcalc/app.py
@@ -10,18 +10,22 @@ app = Flask(__name__)
 def homepage():
 
     data_parser = DataParser()
-    selected_day = '20231229'
+    selected_day = 'days/20240101'
     selected_field = 'internal_supply'
     initial_charge = 10000
     capacity = 15000
+    price_feed_in = 0.05
+    price_external = 0.405
 
     if request.method == 'POST':
         if 'selected_field' in request.form:
             # Get the value entered in the form field
             selected_field = request.form['selected_field']
             selected_day = request.form['selected_day']
-            initial_charge = float(request.form['initial_charge'])
-            capacity = float(request.form['capacity'])
+            initial_charge = float(request.form['initial_charge']) * 1000
+            capacity = float(request.form['capacity']) * 1000
+            price_feed_in = float(request.form['price_feed_in'])
+            price_external = float(request.form['price_external'])
 
 
     # Get data based on the selected field
@@ -34,9 +38,19 @@ def homepage():
     data_external = data_parser.data_by_field('consumption').tolist()
     feed_in = data_parser.data_by_field('feed-in').tolist()
     external_supply = data_parser.data_by_field('external_supply').tolist()
-    sum_feed_in = sum(feed_in) * data_parser.interval
-    sum_external = sum(external_supply) * data_parser.interval
+    sum_feed_in = sum(feed_in) * data_parser.interval / 1000
+    sum_external = sum(external_supply) * data_parser.interval / 1000
     storage.set_production_consumption(data_internal, data_external)
+    sum_feed_in_battery = storage.sum_feed_in / 1000
+    sum_external_battery = storage.sum_external / 1000
+
+
+    revenu_feed_in = price_feed_in * sum_feed_in
+    costs_external = price_external * sum_external
+    revenu_feed_in_battery = price_feed_in * sum_feed_in_battery
+    costs_external_battery = price_external * sum_external_battery
+
+    gain = revenu_feed_in_battery - revenu_feed_in - costs_external_battery + costs_external
 
 
 
@@ -53,10 +67,17 @@ def homepage():
         capacity=capacity,
         selected_field=selected_field,
         selected_day=selected_day,
-        sum_feed_in_battery=storage.sum_feed_in,
-        sum_external_battery=storage.sum_external,
+        price_feed_in=price_feed_in,
+        price_external=price_external,
+        sum_feed_in_battery=sum_feed_in_battery,
+        sum_external_battery=sum_external_battery,
         sum_feed_in=sum_feed_in,
         sum_external=sum_external,
+        costs_external=costs_external,
+        costs_external_battery=costs_external_battery,
+        revenue_feed_in=revenu_feed_in,
+        revenue_feed_in_battery=revenu_feed_in_battery,
+        gain=gain,
     )
 
 
